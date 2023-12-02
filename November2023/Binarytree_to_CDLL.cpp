06f16bf53c1d1eb7fe305423f87a99253de4ad8b
Anushka Joshi <79532982+Anushka0902@users.noreply.github.com>
2023-12-02 15:31:58 +0530

    Create Binarytree_to_CDLL.cpp

---
+++ November2023/Binarytree_to_CDLL.cpp
@@ -0,0 +1,28 @@
+ void inorder(Node *root) {
+        if (!root)
+            return;
+
+        inorder(root->left);
+        
+        Node* curr = new Node;
+        curr->data = root->data;
+        if (tail) {
+            tail->right = curr;
+            curr->left = tail;
+        } else 
+            head = curr;
+        tail = curr;
+
+        inorder(root->right);
+    }
+
+    Node *bTreeToCList(Node *root) {
+        tail = head = NULL;
+
+        inorder(root);
+
+        head->left = tail;
+        tail->right = head;
+
+        return head;
+    }
