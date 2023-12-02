c0703c990702b5a471de5274a241b05566bb177c
Anushka Joshi <79532982+Anushka0902@users.noreply.github.com>
2023-12-02 15:40:21 +0530

    Create AVL_insertion.cpp

---
+++ November2023/AVL_insertion.cpp
@@ -0,0 +1,30 @@
+bool isBST(Node *n, int lower, int upper)
+{
+	if(!n) return 1;
+	if( n->data <= lower || n->data >= upper ) return 0;
+	return isBST(n->left, lower, n->data) && isBST(n->right, n->data, upper) ;
+}
+
+pair<int,bool> isBalanced(Node* n)
+{
+	if(!n) return pair<int,bool> (0,1);
+
+	pair<int,bool> l = isBalanced(n->left);
+	pair<int,bool> r = isBalanced(n->right);
+
+	if( abs(l.first - r.first) > 1 ) return pair<int,bool> (0,0);
+
+	return pair<int,bool> ( 1 + max(l.first , r.first) , l.second && r.second );
+}
+
+bool isBalancedBST(Node* root)
+{
+	if( !isBST(root, INT_MIN, INT_MAX) )
+		cout<< "BST voilated, inorder traversal : ";
+
+	else if ( ! isBalanced(root).second )
+		cout<< "Unbalanced BST, inorder traversal : ";
+
+	else return 1;
+	return 0;
+}
