5efecac9b4691f83c97c5d59e44a27e735642335
Anushka Joshi <79532982+Anushka0902@users.noreply.github.com>
2023-12-02 15:42:30 +0530

    Create Print_pattern.cpp

---
+++ November2023/Print_pattern.cpp
@@ -0,0 +1,13 @@
+ void solve(int n, vector<int>& out) {
+        out.push_back(n);
+        if (n <= 0)
+            return;
+        solve(n - 5, out);
+        out.push_back(n);
+    }
+
+    vector<int> pattern(int N) {
+        vector<int> out;
+        solve(N, out);
+        return out;
+    }
