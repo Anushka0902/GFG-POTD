db9b0ad400adb4c66977c7f090a37102a38f2821
Anushka Joshi <79532982+Anushka0902@users.noreply.github.com>
2023-12-07 14:40:26 +0530

    Create 7-number of subarray with max.cpp

---
+++ December2023/7-number of subarray with max.cpp
@@ -0,0 +1,23 @@
+class Solution{
+    public:
+    long countSubarrays(int a[], int n, int L, int R)
+    {
+        // Complete the function
+        
+         long out = 0, range = 0;
+        long i = 0;
+        
+        for (long j = 0; j < n; ++j)
+        {
+            if (a[j] >= L && a[j] <= R)
+                range = j - i + 1;
+            else if (a[j] > R)
+                range = 0, i = j + 1;
+
+            out += range;
+        }
+        
+        return out;
+        
+    }
+};
